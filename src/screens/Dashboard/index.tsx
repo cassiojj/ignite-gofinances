a8c0dcbdf50aba73808f2699012a47b967550fff
CJJ <cassio.jjesus@gmail.com>
2021-06-16 23:08:27 -0300

    Termino estudos do dia

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
 
 import {
   Container,
@@ -9,29 +11,59 @@ import {
   User,
   UserGreting,
   UserName,
-  Icon
-} from './styles'
+  Icon,
+  HighlightCards,
+  Transaction,
+  Title,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-    <Header>
-      <UserWrapper>
-        <UserInfo>
-          <Photo
-            source={{ uri: 'https://avatars.githubusercontent.com/u/17159083?v=4'}}
-          />
-          <User>
-            <UserGreting>Olá, </UserGreting>
-            <UserName>Cassio</UserName>
-          </User>
-        </UserInfo>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/17159083?v=4",
+              }}
+            />
+            <User>
+              <UserGreting>Olá, </UserGreting>
+              <UserName>Cassio</UserName>
+            </User>
+          </UserInfo>
 
-        <Icon name="power"/>
-      </UserWrapper>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
 
-    </Header>
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16,141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
+
+      <Transaction>
+        <Title>Listagem</Title>
+
+        <TransactionCard />
+      </Transaction>
     </Container>
-    )
-  }
-
+  );
+}
